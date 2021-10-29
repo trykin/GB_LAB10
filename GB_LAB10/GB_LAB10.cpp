65dffa58aef9144f79e3d00b17e498670583cb0d
Peter Novikov <peter@p-home.ru>
2021-10-29 23:28:29 +0300

    Добавьте файлы проекта.

---
+++ GB_LAB10/GB_LAB10.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <direct.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	char* dir;
+	char fileName[256];
+	FILE* file;
+	//Читаем текущий каталог
+	if ((dir = _getcwd(NULL, 0)) == NULL)
+		perror("Error read directory");
+	//Склеиваем
+	snprintf(fileName, sizeof fileName, "%s%s", dir, "\\file.csv");
+
+	srand(time(NULL));
+	unsigned int s = rand() % 100;
+	file = fopen(fileName, "w+t");
+	for (int i = 0; i < s; i++)
+		fprintf(file, "%d;%d;%d;%d;\n", rand(), rand(), rand(), rand());
+	fclose(file);
+}
